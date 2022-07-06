a967c5587417b6a5ec5667d45dfebb2ce137ec34
Andrew Tran <tranandrew@gmail.com>
2022-07-06 10:29:27 -0500

    InsertSort comment. MergeSort folder created

--- 10-Algos-BasicSort/Insertion+Sort.js
+++ 10-Algos-BasicSort/Insertion+Sort.js
@@ -3,11 +3,13 @@ function insertionSort(array) {
     // always start off at the second number
     for(let i = 1; i < array.length; i++) {
         temp = array[i];
-        for(let j = i - 1; array[j] > temp && j > -1; j--) {
-            // switch value of array[j+1] to value of array[j]
+        // var is used on purpose here so that the variable can be used out of scope.
+        for(var j = i - 1; array[j] > temp && j > -1; j--) {
+            // switch value of array[j+1] to value of array[j]. This slides the larger number to the right.
+            // When array[j] is not greater than temp, then we will be kicked out of for loop
             array[j+1] = array[j];
         }
-        // switch value of larger index to value of temp
+        // this places temp into the open spot created when we made array[j+1] = array[j] value
         array[j+1] = temp;
     }
     return array;
